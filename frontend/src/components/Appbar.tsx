b96574a7cc8248056f927d8f8df420ec72909f27
Sourya07 <singhsourya137@gmail.com>
2024-09-19 18:20:52 +0530

    some chanhges

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom"
 export const Appbar = () => {
     return <div className="border-b flex justify-between px-10 py-4">
         <Link to={'/blogs'} className="flex flex-col justify-center cursor-pointer">
-            Medium
+            we travel
+
         </Link>
         <div>
             <Link to={`/publish`}>
